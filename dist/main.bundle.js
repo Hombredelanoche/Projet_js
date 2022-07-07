49bf13736fd04260192f4ca45b3995fb74c2a8cc
elHombreDeLaNoche <elbrandonpro@yahoo.Fr>
2022-07-07 12:29:30 +0200

    Projet_JS

---
+++ dist/main.bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var express = "Il était une fois";
+console.log(express);
+/******/ })()
+;
+//# sourceMappingURL=main.bundle.js.map
---
+++ src/index.js
@@ -0,0 +1,7 @@
+var firstName = "Brandon";
+
+const b = new Number(2);
+
+console.log(b);
+
+b.toFixed(2)
